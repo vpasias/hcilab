00550104f33c883420b648385266da7cc29aeee2
vpasias <52623837+vpasias@users.noreply.github.com>
2022-02-05 19:31:38 +0200

    Update vm_deployment.sh

--- vm_deployment.sh
+++ vm_deployment.sh
@@ -80,7 +80,7 @@ ssh -o "StrictHostKeyChecking=no" ubuntu@netsim "sudo sed -i 's/hosts:          
 ssh -o "StrictHostKeyChecking=no" ubuntu@netsim "sudo usermod -aG libvirt ubuntu && sudo adduser ubuntu libvirt-qemu && sudo adduser ubuntu kvm && sudo adduser ubuntu libvirt-dnsmasq && echo 0 | sudo tee /sys/module/kvm/parameters/halt_poll_ns"
 ssh -o "StrictHostKeyChecking=no" ubuntu@netsim "sudo sed -i 's/0770/0777/' /etc/libvirt/libvirtd.conf"
 
-ssh -o "StrictHostKeyChecking=no" ubuntu@netsim "sudo DEBIAN_FRONTEND=noninteractive apt install task-xfce-desktop xrdp --install-recommends -y"
-ssh -o "StrictHostKeyChecking=no" ubuntu@netsim "sudo systemctl start xrdp && sudo systemctl enable xrdp && sleep 10"
+ssh -o "StrictHostKeyChecking=no" ubuntu@netsim "sudo DEBIAN_FRONTEND=noninteractive apt install xrdp tigervnc-standalone-server --install-recommends -y"
+ssh -o "StrictHostKeyChecking=no" ubuntu@netsim "sudo systemctl enable xrdp"
 
 ssh -o "StrictHostKeyChecking=no" ubuntu@netsim "sudo reboot"
